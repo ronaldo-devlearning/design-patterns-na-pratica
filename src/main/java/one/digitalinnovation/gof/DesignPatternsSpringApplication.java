6575aedebcab592791f824cdc91d0ce8f4643974
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-11-19 12:13:29 -0300

    Desafio iniciado

---
+++ src/main/java/one/digitalinnovation/gof/DesignPatternsSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsSpringApplication.class, args);
+	}
+
+}
